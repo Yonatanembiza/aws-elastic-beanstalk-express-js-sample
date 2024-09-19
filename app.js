5a5035608d4b9b32d45f988c374f9f433c1e8f3d
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2024-09-19 12:59:48 -0500

    Change to line number-5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('This is Yonatan - Trying Git, AWS CodeBuild, AWS CodePipeline, AWS Elastic Beanstalk Service '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
